2675dd7faf7048d2ffa84265d54e2bf6291e8972
Petar Scepanovic <hiborija@gmail.com>
2019-07-18 16:55:11 +0200

    added endpoints for fetching all recipes and by id

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -1,1 +1,25 @@
 const db = require('../dbConfig');
+
+
+function find() {
+  return db('recipe');
+}
+
+function findById(id) {
+  return db('recipe').where({ id });
+}
+
+
+function add({ name }) {
+  return db('recipe').insert({ name });
+}
+
+
+
+
+
+module.exports = {
+  find,
+  findById,
+  add,
+};
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -7,6 +7,43 @@ const router = express.Router();
 const db = require('../dbConfig');
 
 
+router.get('/', async (req, res) => {
+  try {
+    const recipes = await Recipes.find();
+    res.json(recipes);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get recipes' });
+  }
+});
+
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const recipe = await Recipes.findById(id);
+
+    if (recipe) {
+      res.json(recipe);
+    } else {
+      res.status(404).json({ message: 'Could not find recipe with given id.' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get recipe' });
+  }
+});
+
+
+router.post('/', async (req, res) => {
+  const recipeData = req.body;
+
+  try {
+    const recipe = await Recipes.add(recipeData);
+    res.status(201).json(recipe);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to create new recipe' });
+  }
+});
 
 
 module.exports = router;
